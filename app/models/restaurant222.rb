148a4cbf150fcb6f792890328efdb3e4194a73ca
Jash Shah <jash.shah@live.com>
2018-06-13 13:07:00 -0300

    Models done.

--- app/models/restaurant222.rb
+++
@@ -1,7 +0,0 @@
-class Restaurant < ApplicationRecord
-  has_many :reviews, dependent: :destroy
-  validates :name, presence: true
-  validates :address, presence: true
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
-
-end
