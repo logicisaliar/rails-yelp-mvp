c93276883b4649714631d4b5aa00be5adc4cea05
Jash Shah <jash.shah@live.com>
2018-06-13 14:35:25 -0300

    All controllers added to restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Restaurant.create(name: 'La Cabrera', phone_number: "81831001", address: "Palermo 1", category: "belgian")
+Restaurant.create(name: 'La Cabrero', phone_number: "81831002", address: "Palermo 2", category: "chinese")
+Restaurant.create(name: 'La Cabreru', phone_number: "81831003", address: "Palermo 3", category: "italian")
+Restaurant.create(name: 'La Cabreri', phone_number: "81831004", address: "Palermo 4", category: "japanese")
+Restaurant.create(name: 'La Cabrere', phone_number: "81831005", address: "Palermo 5", category: "french")
